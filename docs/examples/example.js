e98199a214eeac1d8cf224007399a442e014acee
Cryptid <Wisienki2ie@gmail.com>
2024-08-26 12:30:17 +0200

    updated javascript example

--- docs/examples/example.js
+++ docs/examples/example.js
@@ -3,7 +3,9 @@ const data = {
     model: 'ThingAI 2.0 Pro',
     process_draw_command: "yes",
     messages: [{"role": "user", "content": "Hello ThingAI!"}],
-    database: {"Best AI": "ThingAI", "Favourite color": "purple"}
+    database: {"Best AI": "ThingAI", "Favourite color": "purple"},
+    memories: {"26-08-2024 12:12:39": "The user said that they like the color purple really much"},
+    time: "26.08.2024 12:29:14"
 };
 
 fetch(apiUrl, {
@@ -16,3 +18,5 @@ fetch(apiUrl, {
 .then(response => response.json())
 .then(json => console.log('Response:', json))
 .catch(error => console.error('Error:', error));
+
+
